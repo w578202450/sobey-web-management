26f0d247e7e764600239fd303c9d6c944935f727
wang <578202450.com>
2024-07-24 16:25:28 +0800

    完成数据新增

---
+++ src/common/database/database.providers.ts
@@ -0,0 +1,50 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { getConfig } from 'src/utils/index'
+const path = require('path');
+
+// 设置数据库类型
+const databaseType: DataSourceOptions['type'] = 'mysql';
+const { MYSQL_CONFIG } = getConfig()
+
+const MYSQL_DATABASE_CONFIG = {
+  ...MYSQL_CONFIG,
+  type: databaseType,
+  entities: [path.join(__dirname, `../../**/*.${MYSQL_CONFIG.entities}.entity{.ts,.js}`)],
+}
+
+const MYSQL_DATA_SOURCE = new DataSource(MYSQL_DATABASE_CONFIG)
+
+// 数据库注入
+export const DatabaseProviders = [
+  {
+    provide: 'MYSQL_DATA_SOURCE',
+    useFactory: async () => {
+      await MYSQL_DATA_SOURCE.initialize()
+      return MYSQL_DATA_SOURCE
+    }
+  }
+];
+
+
+// // 设置数据库类型
+// const databaseType: DataSourceOptions['type'] = 'mongodb';
+// const { MONGODB_CONFIG } = getConfig()
+
+// const MONGODB_DATABASE_CONFIG = {
+//   ...MONGODB_CONFIG,
+//   type: databaseType,
+//   entities: [path.join(__dirname, `../../**/*.${MONGODB_CONFIG.entities}.entity{.ts,.js}`)],
+// }
+
+// const MONGODB_DATA_SOURCE = new DataSource(MONGODB_DATABASE_CONFIG)
+
+// // 数据库注入
+// export const DatabaseProviders = [
+//   {
+//     provide: 'MONGODB_DATA_SOURCE',
+//     useFactory: async () => {
+//       await MONGODB_DATA_SOURCE.initialize()
+//       return MONGODB_DATA_SOURCE
+//     }
+//   }
+// ];
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+import { DatabaseModule } from '@/common/database/database.module';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { UserProviders } from './user.providers';
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [
+    DatabaseModule
+  ],
+  controllers: [
+    UserController
+  ],
+  providers: [...UserProviders, UserService],
+  exports: [UserService],
 })
-export class UserModule {}
+export class UserModule { }
